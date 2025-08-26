26a4d434c0c298ae4d28ae655d2da012bf6be2f4
Franco Di Monda <137169548+FrancoGitHub2023@users.noreply.github.com>
2025-08-27 00:26:01 +0200

    Update PostsList.jsx

--- src/pages/PostsList.jsx
+++ src/pages/PostsList.jsx
@@ -1,4 +1,3 @@
-// src/pages/PostsList.jsx
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -280,5 +279,6 @@ const PostsList = () => {
         </div>
     );
 };
+
 
 export default PostsList;
